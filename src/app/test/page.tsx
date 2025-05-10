22f5f4695b6f3b5861eb1f30565390497ed7fc20
zer0 <zer0.garden.0117@gmail.com>
2025-05-11 00:05:05 +0900

    テストページ削除

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TextAnimate } from '@gfazioli/mantine-text-animate';
-
-export default function Demo() {
-  return (
-    <TextAnimate animate="in" animation="slideUp" by="character">
-      Mantine TextAnimate component
-    </TextAnimate>
-  );
-}
